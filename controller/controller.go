31492a63e7aae648974bef9c23403c238212fb0a
courtsvii <courtneyedwards@Courtneys-MacBook-Air.local>
2019-03-22 08:37:31 +0000

    Small Improvements

--- controller/controller.go
+++ controller/controller.go
@@ -9,10 +9,10 @@ import (
 
 func main() {
 	http.HandleFunc("/v1/hdfs/mv", hdfs.Mv)
-	http.HandleFunc("/v1/running/", up)
+	http.HandleFunc("/v1/up/", up)
 	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
 }
 
 func up(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Service Up!")
+	fmt.Fprintf(w, "Service Up! \n")
 }
---
+++ run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse, sys, docker
+
+def get_free_port():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("",0))
+    free_port = s.getsockname()[1]
+    s.close
+    return free_port
+
+parser=argparse.ArgumentParser()
+
+parser.add_argument('--hadoop_address', '-a', help='the address:port combination used to access your hadoop cluster', type=str, default="0.0.0.0:9000")
+parser.add_argument('--port', '-p', help='port to expose your container on', type=int, default=get_free_port())
+parser.add_argument('--detach', '-d', help='run container in detached mode or not', type=bool, default=True)
+parser.add_argument('--tag', '-t', help='tag for your container', type=str, default="go-webhdfs")
+
+args=parser.parse_args()
+
+print ("hadoop_address has been set to {}".format(args.hadoop_address)) 
+print ("port has been set to {}".format(args.port)) 
+
+docker_client = docker.from_env()
+tag = "go-webhdfs"
+buildargs = {'DOCKER_HADOOP_ADDRESS' : args.hadoop_address}
+print("buiding go-webhdfs image. tag = {}, buildargs = {}".format(tag, buildargs))
+docker_client.images.build(path=".", tag=tag, buildargs=buildargs)
+
+print("running go-webhdfs container. detach = {} ports = {}".format(args.detach, args.port))
+docker_client.containers.run("go-webhdfs", detach=args.detach, ports =  {'8000': args.port})
+
+print("go-webhdfs is running on port: {}".format(args.port))
