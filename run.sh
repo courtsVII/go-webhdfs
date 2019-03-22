e1a18fe25270bf411403038bdc46d6965b4b8bd7
courtsvii <courtneyedwards@Courtneys-MacBook-Air.local>
2019-03-22 00:38:32 +0000

    Small Improvements

--- run.sh
+++ run.sh
@@ -3,12 +3,16 @@
 set -e
 
 HADOOP_ADDRESS="0.0.0.0:9000"
+PORT=""
 
-while getopts "a:" opt; do
+while getopts ":a:p:" opt; do
   case ${opt} in
     a ) 
         HADOOP_ADDRESS=${OPTARG}
       ;;
+    p ) 
+        PORT=${OPTARG}
+    ;;
   esac
 done
 
@@ -19,12 +23,24 @@ echo "hadoop address set to: ${HADOOP_ADDRESS}"
 docker build -t go-webhdfs --build-arg DOCKER_HADOOP_ADDRESS=${HADOOP_ADDRESS} . 
 
 function get_unused_port() {
-  for port in $(seq 4444 65000);
-  do
-    [ $? -eq 1 ] && echo "$port" && break;
+  LPORT=32768;
+  UPORT=60999;
+  while true; do
+      MPORT=$[$LPORT + ($RANDOM % $UPORT)];
+      (echo "" >/dev/tcp/127.0.0.1/${MPORT}) >/dev/null 2>&1
+      if [ $? -ne 0 ]; then
+          echo $MPORT;
+          return 0;
+      fi
   done
 }
-PORT="$(get_unused_port)"
+
+if [[ -z "${PORT}" ]]
+then
+      PORT="$(get_unused_port)"
+fi
+
+
 
 docker run -p ${PORT}:8000 --detach go-webhdfs  
 echo "go-webhdfs is running on port: ${PORT}"
