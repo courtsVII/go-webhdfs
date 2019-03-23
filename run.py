004058ff5af2c2fc2ba7d3ef3cec5f6fba0594c9
courtsvii <courtneyedwards@Courtneys-MacBook-Air.local>
2019-03-23 07:09:55 +0000

    setup.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import argparse, sys, docker
+import argparse, sys, docker, os
 
 def get_free_port():
     import socket
@@ -10,6 +10,12 @@ def get_free_port():
     s.close
     return free_port
 
+def chdir_to_scripts_folder():
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+
 parser=argparse.ArgumentParser()
 
 parser.add_argument('--hadoop_address', '-a', help='the address:port combination used to access your hadoop cluster', type=str, default="0.0.0.0:9000")
@@ -18,6 +24,9 @@ parser.add_argument('--detach', '-d', help='run container in detached mode or no
 parser.add_argument('--tag', '-t', help='tag for your container', type=str, default="go-webhdfs")
 
 args=parser.parse_args()
+
+chdir_to_scripts_folder()
+
 
 print ("hadoop_address has been set to {}".format(args.hadoop_address)) 
 print ("port has been set to {}".format(args.port)) 
